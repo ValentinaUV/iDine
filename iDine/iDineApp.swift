c2d1301652e4b51c6127b8c2c3de20831ea778fb
Valentina <galaxyspevv@gmail.com>
2022-02-01 18:54:26 +0200

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Ungurean Valentina on 01.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
